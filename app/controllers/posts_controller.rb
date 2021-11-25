aad1b471823728e36e93626703f43a1c5728563f
daichi kouno <15214kouno@yuai.ac.jp>
2021-11-25 11:23:11 +0900

    postモデルにバリデーションを追加、投稿一覧ページ作成、新規投稿機能追加、コレクションをレンダリング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.page(params[:page]).per(10)
   end
 
   def create
+    post = Post.create!(body:params[:body])
+   redirect_to root_path  
   end
 end
