1ec052d7b374c1fe32764c305c0004283a909a30
daichi kouno <15214kouno@yuai.ac.jp>
2021-12-03 21:25:46 +0900

    ストロングパラメータを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,16 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.page(params[:page]).per(10)
+    Post.new
+    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def create
-    post = Post.create!(body:params[:body])
-   redirect_to root_path  
+    Post.create!(body:params[:body])
+    redirect_to root_path  
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
